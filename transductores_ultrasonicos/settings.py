1857b4fc37d207af5b8d66ec3177cbf7875a2ff2
Cristóbal Zenteno <cristobal.zenteno.1@gmail.com>
2023-10-16 00:13:42 -0300

    Iconos redirigen

--- transductores_ultrasonicos/settings.py
+++ transductores_ultrasonicos/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'transductores_ultrasonicos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'consu', ##nombrebd
+        'NAME': 'db2', ##nombrebd
         'USER': 'postgres',
-        'PASSWORD': 'AguanteMessi2023$',  ##passdb
+        'PASSWORD': 'toto',  ##passdb
         'HOST': 'localhost',
         'PORT': '5432',
     },
