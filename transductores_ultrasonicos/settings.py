d9f88800658c7d146e3659ce82c3233eef4202b3
zajhe <jiame0418@gmail.com>
2023-10-17 22:49:45 -0300

    Guardar experimento TXT

--- transductores_ultrasonicos/settings.py
+++ transductores_ultrasonicos/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'transductores_ultrasonicos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db2', ##nombrebd
+        'NAME': 'trans', ##nombrebd
         'USER': 'postgres',
-        'PASSWORD': 'toto',  ##passdb
+        'PASSWORD': 'yumbel1318',  ##passdb
         'HOST': 'localhost',
         'PORT': '5432',
     },
