ecc373a8d5712d2326f05313f0a3ed98dcdabed5
zajhe <juan.martinez2@cloud.uautonoma.cl>
2023-10-28 23:39:43 -0300

    Experiment model

--- experimentos/models.py
+++ experimentos/models.py
@@ -12,13 +12,14 @@ class Experimentos(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     fluido = models.ForeignKey(Fluido, on_delete=models.CASCADE)
     nombre_experimento = models.CharField(max_length=200, null=False)
-    corriente = models.FloatField(null=False)
     voltaje = models.FloatField(null=False)
     frecuencia = models.FloatField(null=True)
+    final_frecuency = models.FloatField(null=True)
     tiempo = models.TimeField(null=True)
     sensibilidad = models.FloatField(null=True)
     pasos = models.FloatField(null=True)
     fecha_experimento = models.DateField(null=False, auto_now_add=True)
+    comentario = models.CharField(max_length=500, null=True)
     pdf_experimento = models.FileField(upload_to='experimento_pdf/', null=True, blank=True)
     tiempo_pausa = models.DurationField(default=timedelta(seconds=0))  # Valor predeterminado de 0 segundos
 
