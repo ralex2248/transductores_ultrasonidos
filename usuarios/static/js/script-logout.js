365e2875d546b4d2068b1df9c526a9136e2b2f17
BriteChile <127158176+BriteChile@users.noreply.github.com>
2023-10-30 00:00:15 -0300

    Dark Mode

--- usuarios/static/js/script-logout.js
+++ usuarios/static/js/script-logout.js
@@ -2,3 +2,30 @@ document.getElementById("logoutSpan").addEventListener("click", function() {
     window.location.href = logoutUrl;
 });
 
+if (document.cookie.replace(/(?:(?:^|.*;\s*)dark-mode\s*\=\s*([^;]*).*$)|^.*$/, "$1") === "enabled") {
+    document.body.classList.add("dark-mode");
+    // Update the toggle button
+    var button = document.getElementById("dark-mode-toggle");
+    button.innerHTML = ' Modo Claro';
+    button.classList.add("dark-mode-button");
+}
+
+// Toggle dark mode
+document.getElementById("dark-mode-toggle").addEventListener("click", function() {
+    var body = document.body;
+    var button = document.getElementById("dark-mode-toggle");
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        document.cookie = "dark-mode=disabled; path=/";
+        // Update the toggle button
+        button.innerHTML = ' Modo Oscuro';
+        button.classList.remove("dark-mode-button");
+    } else {
+        body.classList.add("dark-mode");
+        document.cookie = "dark-mode=enabled; path=/";
+        // Update the toggle button
+        button.innerHTML = ' Modo Claro';
+        button.classList.add("dark-mode-button");
+    }
+});
+
