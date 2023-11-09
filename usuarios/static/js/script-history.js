769323778297a3b90f046e323a21db50dd46146b
zajhe <juan.martinez2@cloud.uautonoma.cl>
2023-11-09 19:34:57 -0300

    Borrar busqueda

--- usuarios/static/js/script-history.js
+++ usuarios/static/js/script-history.js
@@ -25,4 +25,23 @@ document.getElementById('delete_fluidos_button').addEventListener('click', funct
     });
 });
 
+document.getElementById('clearSearchButton').addEventListener('click', function () {
+    document.getElementById('search').value = ''; // Borra el contenido del campo de búsqueda
+    document.getElementById('delete_fluidos_form').submit(); // Envía el formulario para "Ver Todo"
+});
+document.getElementById('search').addEventListener('input', function () {
+    // Muestra el botón "X" si el campo de búsqueda tiene contenido
+    const clearSearchButton = document.getElementById('clearSearchButton');
+    clearSearchButton.style.display = this.value.length ? 'block' : 'none';
+});
+  
 
+document.getElementById('delete_fluidos_button').addEventListener('click', function() {
+    document.getElementById('delete_fluidos_form').action = "{% url 'eliminar_experimentos' %}";
+    document.getElementById('delete_fluidos_form').submit();
+});
+
+document.getElementById('compareButton').addEventListener('click', function() {
+    document.getElementById('delete_fluidos_form').action = "{% url 'comparar_experimentos' %}";
+    document.getElementById('delete_fluidos_form').submit();
+});
